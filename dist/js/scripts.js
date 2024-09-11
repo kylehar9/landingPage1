d5dc2c62ca5d2133d9c20b1809e249440b99a5b3
Kyle Harline <kylehar9@gmail.com>
2024-09-10 22:19:34 -0600

    OK. some progress on style control...

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Business Frontpage v5.0.9 (https://startbootstrap.com/template/business-frontpage)
+* Start Bootstrap - Kyles IS 581 Landing Page v5.0.9 (https://kylehar9.github.io/landingPage1/dist/index.html)
 * Copyright 2013-2024 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/is581-landing-page/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
